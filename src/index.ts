76db4818dc091213154c4e0cbef5e0d01a1cfd18
Ariel Schvartz <ariel@chocolatesoup.ca>
2022-02-10 15:25:16 -0500

    Fixed @types

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from './lib/Entity';
+export * from './lib/Entity/DynamoPaginator';
+export {
+  aliasTo,
+  aliases,
+  compositeKey,
+  hasMany,
+  hasOne,
+  prop,
+  table,
+  validate,
+} from './lib/Decorators/index';
